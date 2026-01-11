6b3df1976ee315dac59ba19b644cb679637b1390
Manoj kumar S <147139472+Mkumar262006@users.noreply.github.com>
2026-01-11 13:33:35 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -212,3 +212,11 @@ def whatsapp_reply():
 # ==================================================
 # START SERVER (CLOUD SAFE)
 # ========
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+
