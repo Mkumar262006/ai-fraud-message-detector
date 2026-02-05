3062320adc7633ab4efdf0b7852bc73fabaa4dfd
Manoj kumar S <147139472+Mkumar262006@users.noreply.github.com>
2026-02-05 18:59:04 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -402,4 +402,5 @@ def export_csv():
     
 # ================= SERVER =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",8080)))
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
